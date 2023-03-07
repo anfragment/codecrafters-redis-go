4ea197c9c55ac3c8523f547e0b3ed82c5fb480f7
grumpydog <me@grumpydog.space>
2023-03-07 20:17:56 +0600

    feat: PING

--- app/server.go
+++ app/server.go
@@ -15,9 +15,19 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		go handleRequest(conn)
 	}
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	conn.Write([]byte("+PONG\r\n"))
 }
